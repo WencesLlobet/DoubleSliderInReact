99d08930511c6551b17c37e66bd17d095707b436
Lau Llobet <lau.llobet@gmail.com>
2019-05-29 11:16:22 +0200

    forms that modify state

--- udemy-course-0/src/App.js
+++ udemy-course-0/src/App.js
@@ -31,7 +31,32 @@ class App extends Component {
           <Button onClick={this.switchContent}>SendOnOffToChild</Button>
           {JSON.stringify(onoff)}
           <HelloWorld isGreat={onoff}/>
+          <Formulario/>
         </div>
+      );
+    }
+}
+
+class Formulario extends Component {
+  constructor(){
+    super();
+    this.state = { hasanA: false, text: "write here" }
+  }
+
+  handleChange = (e) => {
+      const input = e.target.value;
+      this.setState({text: input, hasanA: input.includes("a")})
+  }
+
+  render() {
+        return (
+        <form>
+             <label>
+                Name:
+                <input type="text" name="name" value={this.state.text} onChange={this.handleChange}/>
+              </label>
+              {this.state.hasanA ? <div>has an A</div> : null}
+          </form>
       );
     }
 }
