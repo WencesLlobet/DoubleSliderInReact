ebaf503f9dab09a6ba02ef3893a5254f132a755d
Lau Llobet <lau.llobet@gmail.com>
2019-05-29 18:15:15 +0200

    BS: searches gifs from the box

--- lyricalic/src/components/SearchForm.js
+++ lyricalic/src/components/SearchForm.js
@@ -2,17 +2,20 @@ import React,{Component} from 'react';
 
 export class SearchForm extends Component{
 
+ songTyped = (e) => {
+   this.setState({ song: e.target.value})
+ }
  render() {return (<div className="searchForm-wrapper">
       <div className="field has-addons">
         <div className="control">
           <input className="input" type="text" placeholder="Group"/>
         </div>
         <div className="control">
-          <input class="input" type="text" placeholder="Song"/>
+          <input onChange={this.songTyped} class="input" type="text" placeholder="Song"/>
         </div>
         <div className="control">
           <button className="button is-info" onClick={( )=>{
-              this.props.songReceived("people, all the people moving")}
+              this.props.songReceived(this.state.song)}
           }>
             Search
           </button>
