36159b7d7a4ff5569f24824de7ac1c417c179eba
Lau Llobet <lau.llobet@gmail.com>
2019-05-29 18:07:44 +0200

    BS: fetches images

--- lyricalic/src/App.js
+++ lyricalic/src/App.js
@@ -8,6 +8,16 @@ import {SearchForm} from './components/SearchForm'
 
 export class App extends Component {
   
+  constructor(){
+    super()
+    fetch('http://api.giphy.com/v1/gifs/search?api_key=***REMOVED***&q=beethoven&limit=1')
+    .then(response => response.json())
+    .then( (json)=>{
+      //this.setState({ url: 'https://media2.giphy.com/media/10qoDO5ERnWoBG/100.gif'})
+      this.setState({url: json.data[0].images.fixed_height_small.url})
+    })
+    this.state = { url: 'https://www.gstatic.com/webp/gallery3/1.sm.png'}
+  }
   songReceived = (song) => {
     alert(song)
   }
@@ -16,7 +26,12 @@ export class App extends Component {
     return (
       <div className="App">
           <SearchForm songReceived={this.songReceived}/>
+          <img src={this.state.url}></img>
       </div>
+
+
+//
+//http://api.giphy.com/v1/gifs/search?api_key=***REMOVED***&q=bach&limit=1
     );
   }
 }
