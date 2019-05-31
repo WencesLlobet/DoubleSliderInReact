dec1c4b3f46af5026d32c224147f9bb4fe906968
Lau Llobet <lau.llobet@gmail.com>
2019-05-31 11:35:41 +0200

    Refactor

--- lyricalic/src/App.js
+++ lyricalic/src/App.js
@@ -7,8 +7,24 @@ export class App extends Component {
   
   constructor(){
     super()
-    this.state = { song: "cat dog", url: 'https://www.gstatic.com/webp/gallery3/1.sm.png'}
+    this.state = { song: "cat dog"}
   }
+
+  componentDidMount() {
+      this.songToGifsMosaicState(this.state.song)
+  }
+  songReceived = (song) => {
+    this.setState({song: song})
+    this.songToGifsMosaicState(song)
+  }
+
+  songToGifsMosaicState = () => {
+    this.state.song.split(" ").map( (word) => {
+      this.getGifForWord(word, (gifUrl)=>{
+          this.setState({['songUrl_'+word]: gifUrl})})
+    });
+  }
+  
   getGifForWord = (word, callback)=> {
     fetch('http://api.giphy.com/v1/gifs/search?api_key='+process.env.REACT_APP_GIPHY_TOKEN+'&q='+word+'&limit=1')
       .then(response => response.json())
@@ -16,13 +32,7 @@ export class App extends Component {
         callback(json.data[0].images.fixed_width_small.url);
       })
   }
-  songReceived = (song) => {
-    this.setState({song: song})
-    song.split(" ").map( (word) => {
-      this.getGifForWord(word, (gifUrl)=>{
-          this.setState({['songUrl_'+word]: gifUrl})})
-    });
-  }
+
   render ( ){ 
     return (
       <div className="App">
